e30246da9a22d31729500d3c412bd2041f53cc9f
Zinedin04 <zinedin0204@gmail.com>
2025-09-25 12:26:25 +0200

    New Database

--- GameAssetStore/obj/Debug/net8.0/GameAssetStore.AssemblyInfo.cs
+++ GameAssetStore/obj/Debug/net8.0/GameAssetStore.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameAssetStore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+41687e6e3ca619ab2a1a0a44aeb1810e191a17bd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7d004faa882532047630bf1480bc03b05428fdac")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameAssetStore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameAssetStore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
